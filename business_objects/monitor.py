7a2be5c1429a00a06b3d6d76118d423d2ed761a4
SimonDegrafKern <simon.degraf@kern.ai>
2023-02-24 09:21:51 +0100

    Formats Code

--- business_objects/monitor.py
+++ business_objects/monitor.py
@@ -1,6 +1,7 @@
 from . import general
 from .. import enums
 
+
 def get_all_tasks(project_id: None, only_running: False):
     query = f"""
     {get_running_information_source_payloads(project_id, only_running)}
@@ -17,24 +18,35 @@ def get_all_tasks(project_id: None, only_running: False):
     """
     return general.execute_all(query)
 
+
 def get_running_information_source_payloads(project_id: None, only_running: False):
     query = f"""
     SELECT id, 'information_source' as task_type
     FROM information_source_payload
     """
     if project_id and only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}' AND state = '{enums.PayloadState.CREATED.value}'
         """
+        )
     if project_id:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}'
         """
+        )
     if only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE state = '{enums.PayloadState.CREATED.value}'
         """
+        )
     return query
+
 
 def get_running_attribute_calculation_tasks(project_id: None, only_running: False):
     query = f"""
@@ -42,37 +54,57 @@ def get_running_attribute_calculation_tasks(project_id: None, only_running: Fals
     FROM weak_supervision_task
     """
     if project_id and only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}' AND state = '{enums.AttributeState.RUNNING.value}'
         """
+        )
     if project_id:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}'
         """
+        )
     if only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE state = '{enums.AttributeState.RUNNING.value}'
         """
+        )
     return query
+
 
 def get_running_tokenization_tasks(project_id: None, only_running: False):
     query = f"""
     SELECT id, 'tokenization' as task_type
     FROM record_tokenization_task
     """
-    if project_id and only_running: # TODO LOOKUP RUNNING STATE OF TOKENIZATION TASK
-        query = query + f"""
+    if project_id and only_running:
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}' AND state = '{enums.TokenizerTask.STATE_IN_PROGRESS.value}' 
         """
+        )
     if project_id:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}'
         """
+        )
     if only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE state = '{enums.TokenizerTask.STATE_IN_PROGRESS.value}'
         """
+        )
     return query
+
 
 def get_running_embedding_tasks(project_id: None, only_running: False):
     query = f"""
@@ -80,53 +112,82 @@ def get_running_embedding_tasks(project_id: None, only_running: False):
     FROM embedding
     """
     if project_id and only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}' AND state = '{enums.EmbeddingState.ENCODING.value}'
         """
+        )
     if project_id:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}'
         """
+        )
     if only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE state = '{enums.EmbeddingState.ENCODING.value}'
         """
+        )
     return query
+
 
 def get_running_weak_supervision_tasks(project_id: None, only_running: False):
     query = f"""
     SELECT id, 'weak_supervision' as task_type
     FROM weak_supervision_task
     """
-    if project_id and only_running: # TODO LOOKUP Weak Supervision States
-        query = query + f"""
+    if project_id and only_running:
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}' AND state = '{enums.PayloadState.CREATED.value}'
         """
+        )
     if project_id:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}'
         """
+        )
     if only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE state = '{enums.PayloadState.CREATED.value}'
         """
+        )
     return query
+
 
 def get_running_upload_tasks(project_id: None, only_running: False):
     query = f"""
     SELECT id, 'upload' as task_type
     FROM upload_task
     """
-    if project_id and only_running: # TODO LOOKUP Weak Supervision States
-        query = query + f"""
+    if project_id and only_running:
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}' AND state = '{enums.UploadStates.IN_PROGRESS.value}'
         """
+        )
     if project_id:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE project_id = '{project_id}'
         """
+        )
     if only_running:
-        query = query + f"""
+        query = (
+            query
+            + f"""
         WHERE state = '{enums.UploadStates.IN_PROGRESS.value}'
         """
+        )
     return query
