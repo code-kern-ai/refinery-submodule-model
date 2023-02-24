9f23c5abb60908adbd35afc0fe5fb292a40617e7
SimonDegrafKern <simon.degraf@kern.ai>
2023-02-24 11:20:59 +0100

    Adds type hints for admin messages

--- business_objects/admin_message.py
+++ business_objects/admin_message.py
@@ -1,35 +1,49 @@
+from typing import List
 from submodules.model.models import AdminMessage
 from . import general
 from ..session import session
+from datetime import datetime
 
 
-def get(message_id):
+def get(message_id: str) -> AdminMessage:
     return session.query(AdminMessage).filter(AdminMessage.id == message_id).first()
 
 
-def get_all():
+def get_all() -> List[AdminMessage]:
     return session.query(AdminMessage).filter().all()
 
 
-def get_all_active():
+def get_all_active() -> List[AdminMessage]:
     return session.query(AdminMessage).filter(AdminMessage.archived == False).all()
 
 
-def create(text: str, level: str, archive_date: int, created_by: str):
+def create(
+    text: str,
+    level: str,
+    archive_date: int,
+    created_by: str,
+    with_commit: bool = False,
+) -> AdminMessage:
     message = AdminMessage(
         text=text,
         level=level,
         archive_date=archive_date,
         created_by=created_by,
     )
-    general.add(message, True)
+    general.add(message, with_commit)
     return message
 
 
-def archive(message_id: str, archived_by: str, archive_date, archived_reason):
+def archive(
+    message_id: str,
+    archived_by: str,
+    archive_date: datetime,
+    archived_reason: str,
+    with_commit: bool = False,
+) -> None:
     message = get(message_id)
     message.archived = True
     message.archived_by = archived_by
     message.archive_date = archive_date
     message.archived_reason = archived_reason
-    general.commit()
+    general.flush_or_commit(with_commit)
