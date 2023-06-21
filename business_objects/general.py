a1b8691a999d22e59ab997e0d85d6e6a3f0f683a
Simon Degraf <125890757+SimonDegrafKern@users.noreply.github.com>
2023-06-21 14:35:33 +0200

    Embedding providers (#44)
    * Adds agreement, gdpr comliant flsgs and embedding provider support
    
    * Adds created by to embedding
    
    * Adds new functions for embedding recreation
    
    * Removes unnecssary method
    
    * Adds refresh to general
    
    * Adds created by to embedding creation
    
    * Embedding updates
    
    * Changes agreement methods
    
    * Makes embedding uppercase

--- business_objects/general.py
+++ business_objects/general.py
@@ -107,3 +107,7 @@ def test_database_connection() -> Dict[str, Any]:
         result["success"] = False
         result["error"] = str(e.__class__.__name__)
     return result
+
+def refresh(obj: Any) -> Any:
+    session.refresh(obj)
+    return obj
