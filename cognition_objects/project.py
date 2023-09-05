8bb1f9158cc855ad91312462f61b12727e82d8ec
Johannes Hötter <johannes.hoetter@kern.ai>
2023-09-05 21:46:19 +0200

    keep refinery projects information

--- cognition_objects/project.py
+++ cognition_objects/project.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Dict, List, Optional, Tuple
 from ..business_objects import general
 from ..session import session
-from ..models import CognitionProject, Project
+from ..models import CognitionProject
 from .. import enums
 from sqlalchemy import func, alias, Integer
 from sqlalchemy.orm import aliased
@@ -16,21 +16,57 @@ def get(project_id: str) -> CognitionProject:
     )
 
 
+def get_all(org_id) -> List[CognitionProject]:
+    return (
+        session.query(CognitionProject)
+        .filter(CognitionProject.organization_id == org_id)
+        .order_by(CognitionProject.created_at.asc())
+        .all()
+    )
+
+
 def create(
     name: str,
     description: str,
     org_id: str,
     user_id: str,
+    refinery_references_project_id: str,
+    refinery_queries_project_id: str,
+    refinery_relevances_project_id: str,
     with_commit: bool = True,
-    timestamp: Optional[str] = None,
+    created_at: Optional[str] = None,
 ) -> CognitionProject:
-
     project: CognitionProject = CognitionProject(
         name=name,
         description=description,
         organization_id=org_id,
         created_by=user_id,
-        timestamp=timestamp,
+        created_at=created_at,
+        refinery_references_project_id=refinery_references_project_id,
+        refinery_query_project_id=refinery_queries_project_id,
+        refinery_relevance_project_id=refinery_relevances_project_id,
     )
     general.add(project, with_commit)
     return project
+
+
+def update(
+    project_id: str,
+    name: Optional[str] = None,
+    description: Optional[str] = None,
+    with_commit: bool = True,
+) -> CognitionProject:
+    project: CognitionProject = get(project_id)
+    if name is not None:
+        project.name = name
+    if description is not None:
+        project.description = description
+    general.flush_or_commit(with_commit)
+    return project
+
+
+def delete(project_id: str, with_commit: bool = True) -> None:
+    session.query(CognitionProject).filter(
+        CognitionProject.id == project_id,
+    ).delete()
+    general.flush_or_commit(with_commit)
