4872d5543d6969c3fc4cbc8a82f6c527d6935609
FelixKirschKern <felix.kirsch@kern.ai>
2023-12-05 13:14:08 +0100

    removes retriever database module

--- cognition_objects/strategy_step.py
+++ cognition_objects/strategy_step.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 from datetime import datetime
 
 from ..business_objects import general
@@ -39,6 +39,7 @@ def create(
     description: str,
     step_type: str,
     position: int,
+    config: Dict,
     with_commit: bool = True,
     created_at: Optional[datetime] = None,
 ) -> CognitionStrategyStep:
@@ -51,6 +52,7 @@ def create(
         description=description,
         step_type=step_type,
         position=position,
+        config=config,
     )
     general.add(strategy, with_commit)
 
@@ -61,12 +63,15 @@ def update(
     project_id: str,
     strategy_step_id: str,
     position: Optional[int] = None,
+    config: Optional[Dict] = None,
     with_commit: bool = True,
 ) -> CognitionStrategyStep:
     strategy_step: CognitionStrategyStep = get(project_id, strategy_step_id)
 
     if position is not None:
         strategy_step.position = position
+    if config is not None:
+        strategy_step.config = config
 
     general.flush_or_commit(with_commit)
     return strategy_step
