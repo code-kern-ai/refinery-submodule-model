f12485aba2010e61de64660f7a75aa06f9a2932a
Johannes Hötter <johannes.hoetter@kern.ai>
2023-12-29 02:11:18 +0100

    adds team

--- cognition_objects/team_member.py
+++ cognition_objects/team_member.py
@@ -38,20 +38,32 @@ def get_all_by_user_id(user_id: str) -> List[CognitionTeamMember]:
 def create(
     team_id: str,
     user_id: str,
+    created_by: str,
     with_commit: bool = True,
     created_at: Optional[datetime] = None,
 ) -> CognitionTeamMember:
-    team: CognitionTeamMember = CognitionTeamMember(
+    team_member: CognitionTeamMember = CognitionTeamMember(
         created_by=user_id,
         team_id=team_id,
+        user_id=user_id,
         created_at=created_at,
     )
-    general.add(team, with_commit)
-    return team
+    general.add(team_member, with_commit)
+    return team_member
 
 
 def delete(team_member_id: str, with_commit: bool = True) -> None:
     session.query(CognitionTeamMember).filter(
         CognitionTeamMember.id == team_member_id,
+    ).delete()
+    general.flush_or_commit(with_commit)
+
+
+def delete_by_team_and_user_id(
+    team_id: str, user_id: str, with_commit: bool = True
+) -> None:
+    session.query(CognitionTeamMember).filter(
+        CognitionTeamMember.team_id == team_id,
+        CognitionTeamMember.user_id == user_id,
     ).delete()
     general.flush_or_commit(with_commit)
