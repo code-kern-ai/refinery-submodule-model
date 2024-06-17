3da52017139637f9328c9520144f1e618e0110bd
Lina <lina.lumburovska@kern.ai>
2024-06-17 18:03:29 +0200

    LLM config to dataset

--- models.py
+++ models.py
@@ -1443,6 +1443,7 @@ class CognitionMarkdownDataset(Base):
     name = Column(String)
     description = Column(String)
     tokenizer = Column(String)
+    llm_config = Column(JSON)
 
     # might want to index this in the future since it's based on an enum
     category_origin = Column(String)
