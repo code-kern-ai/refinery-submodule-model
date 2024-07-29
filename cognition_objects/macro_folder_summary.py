38954b9c1054f248dbcae917e7d2237372ec1a2e
LennartSchmidtKern <lennart.schmidt@kern.ai>
2024-07-29 17:06:07 +0200

    log

--- cognition_objects/macro_folder_summary.py
+++ cognition_objects/macro_folder_summary.py
@@ -7,20 +7,18 @@ from submodules.model.cognition_objects import (
 
 
 def log_macro_execution_summary(
-    macro_id: str,
+    org_id: str,
+    macro_type: str,
+    processed_files_number: int,
     with_commit: bool = True,
 ):
     id = str(uuid.uuid4())
-
-    macro_object = macro_db_co.get(macro_id)
-    org_id = macro_object.organization_id
-    macro_type = macro_object.macro_type
-
     query = f"""
-    INSERT INTO cognition.macro_execution_summary (id, organization_id, creation_month, macro_type, count)
-    VALUES ('{id}', '{org_id}', date_trunc('month', '{date.today()}'), '{macro_type}', 1)
-    ON CONFLICT ON CONSTRAINT unique_summary DO UPDATE
+    INSERT INTO cognition.macro_execution_summary (id, organization_id, creation_month, macro_type, execution_count, processed_files_count)
+    VALUES ('{id}', '{org_id}', date_trunc('month', '{date.today()}'::timestamp), '{macro_type}', 1, {processed_files_number})
+    ON CONFLICT ON CONSTRAINT unique_macro_summary DO UPDATE
         SET count = macro_execution_summary.count + 1;
+        SET processed_files_count = macro_execution_summary.processed_files_count + {processed_files_number};
     """
     general.execute(query)
     general.flush_or_commit(with_commit)
