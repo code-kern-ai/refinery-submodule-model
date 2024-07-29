dfb42793e0a5178c9a793be69b08f696c46fd27d
lumburovskalina <104008550+lumburovskalina@users.noreply.github.com>
2024-07-29 16:01:21 +0200

    ETL different providers (#105)
    * added func
    
    * adjust func
    
    * adds client type
    
    * Column for openai renamed to env id
    
    * Env variable name updated
    
    * LLM config to dataset
    
    * LLm config on datasets
    
    * LLM config for cognition project
    
    * Added LLM config to project
    
    * Removed unused code
    
    * remove env_var_id
    
    * resolving env name to ids and removing from models
    
    * Smaller fix
    
    * adding env value funcitons
    
    * Fixes issue for saving only one property
    
    * Field renamed
    
    * fixing markdown dataset resolve
    
    * Fix when updating llm_config
    
    * more efficient llm logging
    
    * nested access env vars
    
    * extraction
    
    * Removed unnecessary code
    
    * Added tokenizer to project
    
    * Added tokenizer to project
    
    * change to extraction env var
    
    * Transfering subkeys correctly
    
    * adds and condition
    
    * edit condition
    
    * dataset env var return type change
    
    * Update cognition_objects/environment_variable.py
    
    Co-authored-by: JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
    
    * create_md_llm_log using with commit only
    
    * added comment on dict extension
    
    * Added meta data to markdown file
    
    * Meta data to markdown files
    
    * changes to md llm log
    
    * Extraction state added
    
    ---------
    
    Co-authored-by: anmarhindi <anmar.hindi@kern.ai>
    Co-authored-by: LennartSchmidtKern <lennart.schmidt@kern.ai>
    Co-authored-by: anmarhindi <153752112+anmarhindi@users.noreply.github.com>
    Co-authored-by: JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>

--- enums.py
+++ enums.py
@@ -617,8 +617,14 @@ class LLMProvider(Enum):
     AZURE = "Azure"
 
 
+class OpenAIClientType(Enum):
+    OPEN_AI = "OPEN_AI"
+    AZURE = "AZURE"
+
+
 class CognitionMarkdownFileState(Enum):
     QUEUE = "QUEUE"
+    EXTRACTING = "EXTRACTING"
     TOKENIZING = "TOKENIZING"
     SPLITTING = "SPLITTING"
     TRANSFORMING = "TRANSFORMING"
