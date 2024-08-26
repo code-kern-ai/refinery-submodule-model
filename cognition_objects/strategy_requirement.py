56e7eb643dada210bad346f61a0e8bdf64d95d2c
LennartSchmidtKern <lennart.schmidt@kern.ai>
2024-08-26 18:53:44 +0200

    formatting, recreate mappings as update

--- cognition_objects/strategy_requirement.py
+++ cognition_objects/strategy_requirement.py
@@ -3,40 +3,58 @@ from datetime import datetime
 
 from ..business_objects import general
 from ..session import session
-from ..models import CognitionStrategyRequirement, CognitionStrategyRequirementMappingOption
+from ..models import (
+    CognitionStrategyRequirement,
+    CognitionStrategyRequirementMappingOption,
+)
 from src.controller.strategies.model import (
-    StrategyStepCreation,
     StrategyRequirementCreation,
-    StrategyRequirementMappingOptionCreation,
 )
 
 
 def get(project_id: str, strategy_requirement_id: str) -> CognitionStrategyRequirement:
     return (
         session.query(CognitionStrategyRequirement)
-        .filter(CognitionStrategyRequirement.project_id == project_id, CognitionStrategyRequirement.id == strategy_requirement_id)
+        .filter(
+            CognitionStrategyRequirement.project_id == project_id,
+            CognitionStrategyRequirement.id == strategy_requirement_id,
+        )
         .first()
     )
 
-def get_all_by_strategy(project_id: str, strategy_id: str, filter_input: bool = False, filter_output: bool = False) -> List[CognitionStrategyRequirement]:
+
+def get_all_by_strategy(
+    project_id: str,
+    strategy_id: str,
+    filter_input: bool = False,
+    filter_output: bool = False,
+) -> List[CognitionStrategyRequirement]:
     query = session.query(CognitionStrategyRequirement).filter(
-                CognitionStrategyRequirement.project_id == project_id,
-                CognitionStrategyRequirement.strategy_id == strategy_id,
-            )
-    
+        CognitionStrategyRequirement.project_id == project_id,
+        CognitionStrategyRequirement.strategy_id == strategy_id,
+    )
+
     if filter_input:
         query = query.filter(CognitionStrategyRequirement.is_input == True)
     if filter_output:
         query = query.filter(CognitionStrategyRequirement.is_input == False)
-    
+
     return query.all()
 
-def get_all_mapping_options_of_requirement(project_id: str, strategy_requirement_id: str) -> List[CognitionStrategyRequirementMappingOption]:
+
+def get_all_mapping_options_of_requirement(
+    project_id: str, strategy_requirement_id: str
+) -> List[CognitionStrategyRequirementMappingOption]:
     return (
         session.query(CognitionStrategyRequirementMappingOption)
-        .filter(CognitionStrategyRequirementMappingOption.project_id == project_id, CognitionStrategyRequirementMappingOption.strategy_requirement_id == strategy_requirement_id)
+        .filter(
+            CognitionStrategyRequirementMappingOption.project_id == project_id,
+            CognitionStrategyRequirementMappingOption.strategy_requirement_id
+            == strategy_requirement_id,
+        )
         .all()
     )
+
 
 def create(
     project_id: str,
@@ -61,14 +79,39 @@ def create(
 
     return strategy
 
-def create_many(project_id: str, user_id: str, strategy_id: str, requirements: List[StrategyRequirementCreation], with_commit: bool = True, created_at: Optional[datetime] = None) -> List[CognitionStrategyRequirement]:
+
+def create_many(
+    project_id: str,
+    user_id: str,
+    strategy_id: str,
+    requirements: List[StrategyRequirementCreation],
+    with_commit: bool = True,
+    created_at: Optional[datetime] = None,
+) -> List[CognitionStrategyRequirement]:
     strategy_requirements: List[CognitionStrategyRequirement] = []
     for requirement in requirements:
-        strategy_requirement = create(project_id, user_id, strategy_id, requirement.field, requirement.description, requirement.isInput, with_commit, created_at)
+        strategy_requirement = create(
+            project_id,
+            user_id,
+            strategy_id,
+            requirement.field,
+            requirement.description,
+            requirement.isInput,
+            with_commit,
+            created_at,
+        )
         strategy_requirements.append(strategy_requirement)
-        create_mapping_options(project_id, user_id, strategy_requirement.id, [option.value for option in requirement.mappingOptions], with_commit, created_at)
+        create_mapping_options(
+            project_id,
+            user_id,
+            strategy_requirement.id,
+            [option.value for option in requirement.mappingOptions],
+            with_commit,
+            created_at,
+        )
 
     return strategy_requirements
+
 
 def create_mapping_options(
     project_id: str,
@@ -79,19 +122,35 @@ def create_mapping_options(
     created_at: Optional[datetime] = None,
 ) -> List[CognitionStrategyRequirementMappingOption]:
     mapping_options: List[CognitionStrategyRequirementMappingOption] = []
-    for value in values:    
-        mapping_option: CognitionStrategyRequirementMappingOption = CognitionStrategyRequirementMappingOption(
-            project_id=project_id,
-            created_by=user_id,
-            created_at=created_at,
-            strategy_requirement_id=strategy_requirement_id,
-            value=value,
+    for value in values:
+        mapping_option: CognitionStrategyRequirementMappingOption = (
+            CognitionStrategyRequirementMappingOption(
+                project_id=project_id,
+                created_by=user_id,
+                created_at=created_at,
+                strategy_requirement_id=strategy_requirement_id,
+                value=value,
             )
+        )
         mapping_options.append(mapping_option)
 
     general.add_all(mapping_options, with_commit)
 
     return mapping_options
+
+
+def update_mapping_options(
+    project_id: str,
+    user_id: str,
+    strategy_requirement_id: str,
+    values: List[str],
+    with_commit: bool = True,
+) -> List[CognitionStrategyRequirementMappingOption]:
+    delete_mapping_options(project_id, strategy_requirement_id, False)
+    return create_mapping_options(
+        project_id, user_id, strategy_requirement_id, values, with_commit
+    )
+
 
 def update(
     project_id: str,
@@ -100,34 +159,45 @@ def update(
     description: Optional[str] = None,
     with_commit: bool = True,
 ) -> CognitionStrategyRequirement:
-    strategy_requirement: CognitionStrategyRequirement = get(project_id, strategy_requirement_id)
+    strategy_requirement: CognitionStrategyRequirement = get(
+        project_id, strategy_requirement_id
+    )
 
     if field is not None:
         strategy_requirement.field = field
     if description is not None:
         strategy_requirement.description = description
-   
+
     general.flush_or_commit(with_commit)
     return strategy_requirement
 
-def delete(project_id: str, strategy_requirement_id: str, with_commit: bool = True) -> None:
+
+def delete(
+    project_id: str, strategy_requirement_id: str, with_commit: bool = True
+) -> None:
     session.query(CognitionStrategyRequirement).filter(
         CognitionStrategyRequirement.project_id == project_id,
         CognitionStrategyRequirement.id == strategy_requirement_id,
     ).delete()
     general.flush_or_commit(with_commit)
 
-def delete_all_by_strategy(project_id: str, strategy_id: str, with_commit: bool = True) -> None:
+
+def delete_all_by_strategy(
+    project_id: str, strategy_id: str, with_commit: bool = True
+) -> None:
     session.query(CognitionStrategyRequirement).filter(
         CognitionStrategyRequirement.project_id == project_id,
         CognitionStrategyRequirement.strategy_id == strategy_id,
     ).delete()
     general.flush_or_commit(with_commit)
 
-def delete_mapping_options(project_id: str, strategy_requirement_id: str, with_commit: bool = True) -> None:
+
+def delete_mapping_options(
+    project_id: str, strategy_requirement_id: str, with_commit: bool = True
+) -> None:
     session.query(CognitionStrategyRequirementMappingOption).filter(
         CognitionStrategyRequirementMappingOption.project_id == project_id,
-        CognitionStrategyRequirementMappingOption.strategy_requirement_id == strategy_requirement_id,
+        CognitionStrategyRequirementMappingOption.strategy_requirement_id
+        == strategy_requirement_id,
     ).delete()
     general.flush_or_commit(with_commit)
-
