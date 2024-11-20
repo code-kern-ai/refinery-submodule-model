977b002928b17e2cccff36dabff0aa7fcadde3cd
JWittmeyer <jens.wittmeyer@kern.ai>
2024-11-20 14:19:51 +0100

    Remove organization_id

--- models.py
+++ models.py
@@ -1769,11 +1769,6 @@ class CognitionPipelineVersion(Base):
     __tablename__ = Tablenames.PIPELINE_VERSION.value
     __table_args__ = {"schema": "cognition"}
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    organization_id = Column(
-        UUID(as_uuid=True),
-        ForeignKey(f"{Tablenames.ORGANIZATION.value}.id", ondelete="CASCADE"),
-        index=True,
-    )
     project_id = Column(
         UUID(as_uuid=True),
         ForeignKey(f"cognition.{Tablenames.PROJECT.value}.id", ondelete="CASCADE"),
