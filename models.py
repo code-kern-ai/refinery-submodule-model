e3a398784e28124ad4e9d32e3b8e40307a86a6a7
JWittmeyer <jens.wittmeyer@kern.ai>
2024-11-21 15:24:23 +0100

    Redo fkey

--- models.py
+++ models.py
@@ -1275,7 +1275,9 @@ class CognitionMessage(Base):
     version_id = Column(
         # pipeline version with which the version was created, can be null if the version isn't available anymore (e.g. deleted)
         UUID(as_uuid=True),
-        ForeignKey(f"{Tablenames.PIPELINE_VERSION.value}.id", ondelete="SET NULL"),
+        ForeignKey(
+            f"cognition.{Tablenames.PIPELINE_VERSION.value}.id", ondelete="SET NULL"
+        ),
         index=True,
     )
 
