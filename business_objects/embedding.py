aefb17c32f85030091c5273da63a9c2336936e47
JWittmeyer <jens.wittmeyer@kern.ai>
2025-05-21 15:48:20 +0200

    Adds distinct count options

--- business_objects/embedding.py
+++ business_objects/embedding.py
@@ -9,6 +9,7 @@ from ..session import session
 from .. import enums
 
 from ..util import prevent_sql_injection
+from sqlalchemy import distinct, func
 
 
 ALL_EMBEDDINGS_WHITELIST = {
@@ -587,6 +588,15 @@ def get_tensor_count(embedding_id: str) -> EmbeddingTensor:
     )
 
 
+def get_record_ids_count(embedding_id: str) -> int:
+    # note that this is not the same as tensors since e.g. embedding lists are stored with sub_key
+    return (
+        session.query(func.count(distinct(models.EmbeddingTensor.record_id)))
+        .filter(models.EmbeddingTensor.embedding_id == embedding_id)
+        .scalar()
+    )
+
+
 def get_tensor(
     embedding_id: str, record_id: Optional[str] = None, sub_key: Optional[int] = None
 ) -> EmbeddingTensor:
@@ -660,6 +670,11 @@ def create(
         embedding.additional_data = {}
 
     general.add(embedding, with_commit)
+    print(
+        "--------------------------------------------",
+        embedding.delta_full_recalculation_threshold,
+        flush=True,
+    )
     return embedding
 
 
