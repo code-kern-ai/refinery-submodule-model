74008a9201b402cc877963f477b2b1b8b2d88c7b
LennartSchmidtKern <lennart.schmidt@kern.ai>
2025-06-26 11:00:47 +0200

    delete

--- cognition_objects/integration.py
+++ cognition_objects/integration.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm.attributes import flag_modified
 
 from ..business_objects import general
 from ..session import session
-from ..models import CognitionIntegration, Project, CognitionGroup
+from ..models import CognitionIntegration, CognitionGroup
 from ..enums import (
     CognitionMarkdownFileState,
     CognitionIntegrationType,
@@ -234,30 +234,16 @@ def execution_finished(id: str) -> bool:
     )
 
 
-def delete_many2(
-    ids: List[str],
-    delete_refinery_projects: bool = False,
-    delete_cognition_groups: bool = True,
-    with_commit: bool = True,
+def delete_many(
+    ids: List[str], delete_cognition_groups: bool = True, with_commit: bool = True
 ) -> None:
-    integrations = session.query(CognitionIntegration).filter(
-        CognitionIntegration.id.in_(ids)
-    )
-    if delete_refinery_projects:
-        session.query(Project).filter(
-            Project.id.in_(filter(None, [i.project_id for i in integrations]))
-        ).delete(synchronize_session=False)
-    if delete_cognition_groups:
-        session.query(CognitionGroup).filter(
-            CognitionGroup.meta_data.op("->>")("integration_id").in_(ids)
-        ).delete(synchronize_session=False)
-    integrations.delete(synchronize_session=False)
-
-    
-def delete_many(ids: List[str], with_commit: bool = True) -> None:
     (
         session.query(CognitionIntegration)
         .filter(CognitionIntegration.id.in_(ids))
         .delete(synchronize_session=False)
     )
+    if delete_cognition_groups:
+        session.query(CognitionGroup).filter(
+            CognitionGroup.meta_data.op("->>")("integration_id").in_(ids)
+        ).delete(synchronize_session=False)
     general.flush_or_commit(with_commit)
