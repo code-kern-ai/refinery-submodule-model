e0d1fb41787569faad82f4dcfdf4da17a4eddbf0
andhreljaKern <andrea.hrelja@kern.ai>
2025-06-27 16:54:06 +0200

    fix: query builder for record_ids

--- business_objects/embedding.py
+++ business_objects/embedding.py
@@ -324,7 +324,7 @@ def __build_payload_selector(
                 and data_type != enums.DataTypes.PERMISSION.value
             ):
                 payload_selector += f"'{attr}', (r.\"data\"->>'{attr}')::{data_type}"
-            if data_type == enums.DataTypes.PERMISSION.value:  
+            if data_type == enums.DataTypes.PERMISSION.value:
                 payload_selector += f"'{attr}', r.\"data\"->'{attr}'"
             else:
                 payload_selector += f"'{attr}', r.\"data\"->>'{attr}'"
@@ -394,7 +394,8 @@ def get_tensors_and_attributes_for_qdrant(
     WHERE et.project_id = '{project_id}' AND et.embedding_id = '{embedding_id}'
     """
     if record_ids:
-        query += f" AND r.id IN ('{','.join(record_ids)}')"
+        _record_ids = "','".join(record_ids)
+        query += f" AND r.id IN ('{_record_ids}')"
 
     return general.execute_all(query)
 
