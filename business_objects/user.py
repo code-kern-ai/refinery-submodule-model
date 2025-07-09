09afb19223967c96fb615480d83251c202fe2fb8
JWittmeyer <jens.wittmeyer@kern.ai>
2025-07-09 15:43:57 +0200

    fix user with no email

--- business_objects/user.py
+++ business_objects/user.py
@@ -16,7 +16,7 @@ def get(user_id: str) -> User:
 
 def get_user_cached_if_not_admin(user_id: str) -> Optional[User]:
     user = get_user_cached(user_id)
-    if user.email.endswith("@kern.ai") and user.verified:
+    if (user.email or "").endswith("@kern.ai") and user.verified:
         # for admins this could result in two db requests shortly after each other
         # but it's better than having the jumping users without the correct org id
         return get(user_id)
