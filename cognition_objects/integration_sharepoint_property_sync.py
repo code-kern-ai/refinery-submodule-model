4660d9db970cad45c0d560823f49327e15b697f2
LennartSchmidtKern <lennart.schmidt@kern.ai>
2025-07-29 16:22:58 +0200

    get update sync properties

---
+++ cognition_objects/integration_sharepoint_property_sync.py
@@ -0,0 +1,32 @@
+from models import IntegrationSharepointPropertySync
+from typing import List, Optional, Dict, Any
+from sqlalchemy.orm.attributes import flag_modified
+from ..business_objects import general
+from ..session import session
+
+
+def get_by_integration_id(
+    integration_id: str,
+) -> List[IntegrationSharepointPropertySync]:
+    return (
+        session.query(IntegrationSharepointPropertySync)
+        .filter(IntegrationSharepointPropertySync.integration_id == integration_id)
+        .first()
+    )
+
+
+def update(
+    integration_id: str,
+    config: Optional[Dict[str, Any]] = None,
+    logs: Optional[List[Dict[str, Any]]] = None,
+    with_commit: bool = True,
+) -> IntegrationSharepointPropertySync:
+    integration_sync = get_by_integration_id(integration_id)
+    if config is not None:
+        integration_sync.config = config
+        flag_modified(integration_sync, "config")
+    if logs is not None:
+        integration_sync.logs = logs
+        flag_modified(integration_sync, "logs")
+    general.flush_or_commit(with_commit)
+    return integration_sync
