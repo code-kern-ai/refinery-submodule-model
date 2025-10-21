622abccbf919d72220b95e49eda7e495a21c8227
andhreljaKern <andrea.hrelja@kern.ai>
2025-10-21 11:59:01 +0200

    perf: daemon monitoring

--- daemon.py
+++ daemon.py
@@ -1,6 +1,7 @@
 import threading
-from submodules.model.business_objects import general
 import traceback
+from submodules.model.business_objects import general
+from submodules.model import telemetry
 
 
 def run_without_db_token(target, *args, **kwargs):
@@ -8,10 +9,37 @@ def run_without_db_token(target, *args, **kwargs):
     DB session token isn't automatically created.
     You can still do this with general.get_ctx_token but need to return it yourself with remove_and_refresh_session.
     """
+    fn_name = f"{target.__module__}.{target.__name__}"
+
+    def wrapper():
+        telemetry.TASK_RUNNING.labels(
+            app_name=telemetry.APP_NAME,
+            task_name=fn_name,
+        ).inc()
+
+        try:
+            target(*args, **kwargs)
+        except Exception:
+            telemetry.TASK_ERRORS.labels(
+                app_name=telemetry.APP_NAME,
+                task_name=fn_name,
+            ).inc()
+            print("=== Exception in thread ===", flush=True)
+            print(traceback.format_exc(), flush=True)
+            print("===========================", flush=True)
+        else:
+            telemetry.TASK_PROCESSED.labels(
+                app_name=telemetry.APP_NAME,
+                task_name=fn_name,
+            ).inc()
+        finally:
+            telemetry.TASK_RUNNING.labels(
+                app_name=telemetry.APP_NAME,
+                task_name=fn_name,
+            ).dec()
+
     threading.Thread(
-        target=target,
-        args=args,
-        kwargs=kwargs,
+        target=wrapper,
         daemon=True,
     ).start()
 
@@ -21,18 +49,37 @@ def run_with_db_token(target, *args, **kwargs):
     DB session token is automatically created & returned at the end.
     Long running threads needs to occasionally daemon.reset_session_token_in_thread to ensure the session doesn't get a timeout.
     """
+    fn_name = f"{target.__module__}.{target.__name__}"
 
     # this is a workaround to set the token in the actual thread context
     def wrapper():
         general.get_ctx_token()
+        telemetry.TASK_RUNNING.labels(
+            app_name=telemetry.APP_NAME,
+            task_name=fn_name,
+        ).inc()
+
         try:
             target(*args, **kwargs)
         except Exception:
+            telemetry.TASK_ERRORS.labels(
+                app_name=telemetry.APP_NAME,
+                task_name=fn_name,
+            ).inc()
             print("=== Exception in thread ===", flush=True)
             print(traceback.format_exc(), flush=True)
             print("===========================", flush=True)
+        else:
+            telemetry.TASK_PROCESSED.labels(
+                app_name=telemetry.APP_NAME,
+                task_name=fn_name,
+            ).inc()
         finally:
             general.remove_and_refresh_session()
+            telemetry.TASK_RUNNING.labels(
+                app_name=telemetry.APP_NAME,
+                task_name=fn_name,
+            ).dec()
 
     threading.Thread(
         target=wrapper,
