b12d9224b32fd5c240cd89efa893c52533ab3e3c
LennartSchmidtKern <lennart.schmidt@kern.ai>
2025-11-19 18:08:18 +0100

    remove org filter

--- global_objects/inbox_mail.py
+++ global_objects/inbox_mail.py
@@ -91,7 +91,6 @@ def get_by_thread(
         ).filter(
             InboxMailThreadAssociation.user_id == str(user_id),
             InboxMailThread.id == thread_id,
-            InboxMailThread.organization_id == org_id,
         )
 
     inbox_mail_entities = query.order_by(asc(InboxMail.created_at)).all()
@@ -140,10 +139,7 @@ def get_overview_by_threads(
         query = base_query.join(
             InboxMailThreadAssociation,
             InboxMailThreadAssociation.thread_id == InboxMailThread.id,
-        ).filter(
-            InboxMailThreadAssociation.user_id == str(user_id),
-            InboxMailThread.organization_id == org_id,
-        )
+        ).filter(InboxMailThreadAssociation.user_id == str(user_id))
 
     total_threads = query.count()
 
