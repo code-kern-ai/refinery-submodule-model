6e276712e8f9b7e365de9b905d579b2c173829d6
lumburovskalina <104008550+lumburovskalina@users.noreply.github.com>
2026-04-30 14:31:35 +0200

    Org filter on the users view (#243)
    * Org filter on the users view
    
    * PR comments

--- business_objects/user.py
+++ business_objects/user.py
@@ -27,11 +27,7 @@ def get_user_cached_if_not_admin(user_id: str) -> Optional[User]:
 
 
 def get_admin_users() -> List[User]:
-    kernai_admins = (
-        session.query(User)
-        .filter(User.is_admin == True)
-        .all()
-    )
+    kernai_admins = session.query(User).filter(User.is_admin == True).all()
 
     query = """
     SELECT email FROM global.full_admin_access
@@ -231,6 +227,7 @@ def get_active_users_after_filter(
     sort_direction: Optional[str] = None,
     offset: Optional[int] = None,
     limit: Optional[int] = None,
+    filter_organization_id: Optional[str] = None,
 ) -> List[User]:
     last_interaction_range = prevent_sql_injection(
         last_interaction_range, isinstance(last_interaction_range, datetime)
@@ -249,6 +246,12 @@ def get_active_users_after_filter(
         ON u.organization_id = o.id
     WHERE u.email IS NOT NULL
     """
+
+    if filter_organization_id:
+        filter_organization_id = prevent_sql_injection(
+            filter_organization_id, isinstance(filter_organization_id, str)
+        )
+        query += f"\nAND u.organization_id = '{filter_organization_id}'"
 
     if last_interaction_range:
         query += f"\nAND last_interaction >= '{last_interaction_range}'"
